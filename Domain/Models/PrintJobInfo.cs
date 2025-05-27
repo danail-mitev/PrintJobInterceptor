fd1714e793c1a68372e629aa7454ea9eb290abcc
Danail Mitev <danail.mitev96@gmail.com>
2025-05-27 19:08:13 +0200

    Add project files.

---
+++ Domain/Models/PrintJobInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Models
+{
+    public class PrintJobInfo
+    {
+        public string JobId { get; set; } = string.Empty;
+        public string DocumentName { get; set; } = string.Empty;
+        public string User { get; set; } = string.Empty;
+        public string PrinterName { get; set; } = string.Empty;
+        public int TotalPages { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public DateTime TimeSubmitted { get; set; }
+    }
+}
